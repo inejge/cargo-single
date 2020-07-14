3c4bbe833e6156fa85d068e1ce9da6c657aa8d52
Ivan Nejgebauer <inejge@gmail.com>
2020-07-14 14:43:02 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,84 @@
+# cargo-single
+
+To write a relatively simple Rust program, which can fit in a single source
+file but does need a couple of external dependencies, one must use Cargo to
+create a project for the program. Cargo's defaults and tools like `cargo-edit`
+help, but it's still some amount of ceremony and increased friction. This tool
+lets one list the dependencies in the comments at the top of the source file,
+and use that list and the file name to automatically generate the project
+directory, which is then transparently used to check, build or run the program.
+
+## Installation
+
+You must have Rust and Cargo installed and working. Run:
+
+```sh
+cargo install cargo-single
+```
+
+See the [Cargo documentation](https://doc.rust-lang.org/cargo/) to learn how
+`cargo install` works and how to set up your system to find the installed
+binaries.
+
+## Example
+
+Create the source file for your program; as an example, save the following
+as `random.rs`.
+
+```rust
+// rand = "0.7"
+
+use rand::Rng;
+
+fn main() {
+    println!("{}", rand::thread_rng().gen_range(1, 11));
+}
+```
+
+List the dependencies as comments at the top of the file. Each dependency line
+must start with the string `"// "` from the leftmost column, and continue in the
+format used in the `[dependencies]` section of `Cargo.toml`. End the list of
+dependencies with a blank line.
+
+To build and execute the program, run:
+
+```sh
+cargo single run random.rs
+```
+
+## Usage
+
+The tool is invoked through Cargo, with the syntax:
+
+```sh
+cargo single <command> [<option> ...] {<source-file>|<source-dir>} [<arguments>]
+```
+
+_Command_ is one of: __build__, __check__, __refresh__, or __run__. __Refresh__ will
+re-read the source file and update the dependencies in `Cargo.toml`, while the remaining
+three are regular Cargo sub-commands which will be passed to Cargo.
+
+_Options_ are a subset of options accepted by Cargo subcommands. The ones recognized by
+`cargo-single` are:
+
+* __+toolchain__: Name of a toolchain which will be used for building.
+
+* __--release__: Build in release mode.
+
+* __--target *target*__: Use the specified target for building.
+
+* __--no-quiet__: Don't pass `--quiet` to Cargo.
+
+Either the name of the source file, with the `.rs` extension, or of the project directory,
+which has the same name without the extension, must be given to identify the program.
+
+The remaining arguments, if any, will be passed to the program if it's executed.
+
+## License
+
+Licensed under either of:
+
+ * Apache License, Version 2.0 ([LICENSE-APACHE](LICENSE-APACHE)), or
+ * MIT license ([LICENSE-MIT](LICENSE-MIT))
+
+at your option.
---
+++ src/main.rs
@@ -0,0 +1,249 @@
+use std::collections::HashSet;
+use std::env;
+use std::error::Error;
+use std::fs::{self, File};
+use std::io::{BufRead, BufReader, BufWriter, Write};
+use std::path::PathBuf;
+use std::process::{self, Command};
+
+const USAGE: &str = r#"Usage:
+    cargo-single <command> [<option> ...] {<source-file>|<source-dir>} [<arguments>]
+
+<command> is one of: build, check, refresh, run
+    "build", "check" and "run" are regular Cargo subcommands.
+    "refresh" will re-read the source file and update the dependencies in Cargo.toml.
+
+<option> is one or more of:
+    +<toolchain>                Name of a toolchain installed with Rustup.
+    --release                   Build/check in release mode.
+    --target <target>           Use the specified target for building.
+    --no-quiet                  Don't pass --quiet to Cargo."#;
+
+fn fatal_exit(message: &str) -> ! {
+    eprintln!("{}", message);
+    process::exit(1);
+}
+
+#[derive(PartialEq, Eq, Hash)]
+enum CargoOpts {
+    Release,
+    Target,
+    Toolchain,
+}
+
+fn main() {
+    let mut args = env::args();
+    args.nth(1);
+    let cmd = match args.next() {
+        Some(cmd) => cmd,
+        None => fatal_exit(USAGE),
+    };
+    let mut refresh_deps = false;
+    match cmd.as_str() {
+        "build" | "check" | "run" => (),
+        "refresh" => refresh_deps = true,
+        _ => fatal_exit(USAGE),
+    }
+    let mut cargo_args = vec![];
+    let mut cargo_args_seen = HashSet::new();
+    let mut rest = vec![];
+    let mut is_quiet = true;
+    let mut cargo_toolchain = None;
+    while let Some(arg) = args.next() {
+        match arg.as_str() {
+            "--no-quiet" => is_quiet = false,
+            "--release" => {
+                if cargo_args_seen.contains(&CargoOpts::Release) {
+                    fatal_exit("cargo-single: --release already seen");
+                }
+                cargo_args_seen.insert(CargoOpts::Release);
+                cargo_args.push(arg);
+            }
+            "--target" => {
+                if cargo_args_seen.contains(&CargoOpts::Target) {
+                    fatal_exit("cargo-single: --target already seen");
+                }
+                cargo_args_seen.insert(CargoOpts::Target);
+                if let Some(target) = args.next() {
+                    cargo_args.push(arg);
+                    cargo_args.push(target);
+                } else {
+                    fatal_exit("cargo-single: --target needs an argument");
+                }
+            }
+            toolchain if toolchain.starts_with("+") => {
+                if cargo_args_seen.contains(&CargoOpts::Toolchain) {
+                    fatal_exit("cargo-single: toolchain already set");
+                }
+                cargo_args_seen.insert(CargoOpts::Toolchain);
+                cargo_toolchain = Some(arg);
+            }
+            _ => {
+                rest.extend(args.collect::<Vec<_>>());
+                rest.push(arg);
+                break;
+            }
+        }
+    }
+    if rest.is_empty() {
+        fatal_exit(USAGE);
+    }
+    let orig_src = rest.pop().expect("orig src");
+    let mut src = PathBuf::from(&orig_src);
+    let mut file_src = src.clone();
+    match fs::metadata(&src) {
+        Err(e) => {
+            let mut passed = false;
+            if src.extension().unwrap_or_default() != "rs" {
+                file_src.set_extension("rs");
+                if let Ok(md) = fs::metadata(&file_src) {
+                    passed = md.is_file();
+                }
+            }
+            if !passed {
+                fatal_exit(&format!("cargo-single: fatal: {}: {}", orig_src, e));
+            }
+        }
+        Ok(md) if md.is_dir() => {
+            if !file_src.set_extension("rs") {
+                fatal_exit(&format!(
+                    "cargo-single: fatal: {}: cannot set extension",
+                    orig_src
+                ));
+            }
+            match fs::metadata(&file_src) {
+                Err(e) => fatal_exit(&format!(
+                    "cargo-single: fatal: {}: {}",
+                    file_src.to_str().expect("source file"),
+                    e
+                )),
+                Ok(md) if !md.is_file() => {
+                    fatal_exit(&format!(
+                        "cargo-single: fatal: {}: not a regular file",
+                        file_src.to_str().expect("source file")
+                    ));
+                }
+                _ => (),
+            }
+        }
+        _ => (),
+    }
+    src.set_extension("");
+    match fs::metadata(&src) {
+        Ok(md) if !md.is_dir() => {
+            fatal_exit(&format!(
+                "cargo-single: fatal: {}: not a directory",
+                src.to_str().expect("source dir")
+            ));
+        }
+        Ok(_) => (),
+        Err(_) => {
+            let new_args = if is_quiet {
+                &["new", "--quiet", "--bin"][..]
+            } else {
+                &["new", "--bin"][..]
+            };
+            match Command::new("cargo").args(new_args).arg(&src).status() {
+                Err(e) => fatal_exit(&format!(
+                    "cargo-single: error executing \"cargo new\": {}",
+                    e
+                )),
+                Ok(status) if !status.success() => process::exit(1),
+                _ => (),
+            }
+            let mut main_src = src.clone();
+            main_src.push("src");
+            main_src.push("main.rs");
+            if let Err(e) = fs::remove_file(&main_src) {
+                fatal_exit(&format!("cargo-single: error removing main.rs: {}", e));
+            }
+            if let Err(e) = fs::hard_link(&file_src, &main_src) {
+                fatal_exit(&format!(
+                    "cargo-single: error hardlinking to main.rs: {}",
+                    e
+                ));
+            }
+            refresh_deps = true;
+        }
+    }
+    if refresh_deps {
+        let mut cargo_path = src.clone();
+        cargo_path.push("Cargo.toml");
+        let mut cargo_tmp = src.clone();
+        cargo_tmp.push(".Cargo.tmp");
+        if let Err(e) = copy_deps(file_src, cargo_path, cargo_tmp) {
+            fatal_exit(&format!(
+                "cargo-single: error refreshing dependencies: {}",
+                e
+            ));
+        }
+    }
+    if cmd == "refresh" {
+        return;
+    }
+    if is_quiet {
+        cargo_args.push("--quiet".to_owned());
+    }
+    cargo_args.push("--manifest-path".to_owned());
+    src.push("Cargo.toml");
+    cargo_args.push(src.to_str().expect("source dir").to_owned());
+    let mut first_args = vec![];
+    if let Some(toolchain) = cargo_toolchain.as_ref() {
+        first_args.push(toolchain);
+    }
+    first_args.push(&cmd);
+    match Command::new("cargo")
+        .args(first_args)
+        .args(&cargo_args)
+        .arg("--")
+        .args(&rest)
+        .status()
+    {
+        Err(e) => fatal_exit(&format!(
+            "cargo-single: error executing \"cargo {}\": {}",
+            cmd, e
+        )),
+        Ok(status) if !status.success() => process::exit(status.code().unwrap_or(1)),
+        _ => (),
+    }
+}
+
+fn copy_deps(
+    file_src: PathBuf,
+    cargo_path: PathBuf,
+    cargo_tmp: PathBuf,
+) -> Result<(), Box<dyn Error>> {
+    let src = File::open(&file_src)?;
+    let src = BufReader::new(src);
+    let cto = File::open(&cargo_path)?;
+    let cto = BufReader::new(cto);
+    let ctmp = File::create(&cargo_tmp)?;
+    let mut ctmp = BufWriter::new(ctmp);
+    for cto_line in cto.lines() {
+        let cto_line = cto_line?;
+        ctmp.write_all(cto_line.as_bytes())?;
+        ctmp.write_all(b"\n")?;
+        if cto_line != "[dependencies]" {
+            continue;
+        }
+        for src_line in src.lines() {
+            let src_line = src_line?;
+            if !src_line.starts_with("// ") {
+                break;
+            }
+            ctmp.write_all(
+                src_line
+                    .splitn(2, "// ")
+                    .nth(1)
+                    .expect("rest of line")
+                    .as_bytes(),
+            )?;
+            ctmp.write_all(b"\n")?;
+        }
+        ctmp.flush()?;
+        break;
+    }
+    drop(ctmp);
+    fs::rename(&cargo_tmp, &cargo_path)?;
+    Ok(())
+}
